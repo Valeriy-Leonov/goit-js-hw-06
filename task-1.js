56c3312959c5ff475c75d8cc8d76a0646859e280
Valeriy-Leonov <valeoprint@ukr.net>
2020-09-27 06:08:21 +0300

    6

---
+++ task-1.js
@@ -0,0 +1,14 @@
+// Write code under this line
+const getUserNames = array => array.map(user => user.name);
+const UserNames = users.map(({ name }) => ({ name }));
+console.log(getUserNames(users));
+
+/* [
+  "Moore Hensley",
+  "Sharlene Bush",
+  "Ross Vazquez",
+  "Elma Head",
+  "Carey Barr",
+  "Blackburn Dotson",
+  "Sheree Anthony",
+] */
